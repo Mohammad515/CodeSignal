49e58c9fe43e7495a40c46b3cab8a3dbee42e7a8
Mohammad Al-Mahamed <62557147+Mohammad515@users.noreply.github.com>
2020-06-03 13:47:42 +0200

    Update checkPalindrome.java

--- Intro/checkPalindrome.java
+++ Intro/checkPalindrome.java
@@ -2,18 +2,30 @@
  * 
  * Given the string, check if it is a palindrome.
  *
- *  Als Palindrom werden in der Sprachwissenschaft Wörter, Wortreihen oder Sätze bezeichnet, 
- *  die rückwärts gelesen genau denselben Text oder zumindest einen Sinn ergeben.(Wikipedia)
- *
  * Example
  *
  *  For inputString = "aabaa", the output should be checkPalindrome(inputString) = true;
  *  For inputString = "abac", the output should be checkPalindrome(inputString) = false;
  *  For inputString = "a", the output should be checkPalindrome(inputString) = true.
  *
- *  @author Mohammad AlMahamed 
+ ******************************************************************************************
+ *
+ *  Überprüfen Sie, ob der gegebenen String sich um ein Palindrom handelt oder nicht.
+ * 
+ *  Als Palindrom werden in der Sprachwissenschaft Wörter, Wortreihen oder Sätze bezeichnet, 
+ *  die rückwärts gelesen genau denselben Text oder zumindest einen Sinn ergeben.(Wikipedia).
+ *
+ * Beispiele
+ * 
+ * Für inputString = "aabaa" ist die Ausgabe von checkPalindrome(inputString) = true.
+ * Für inputString = "abac" ist die Ausgabe von checkPalindrome(inputString) = false.
+ * Für inputString = "a" ist die Ausgabe von checkPalindrome(inputString) = true.
+ * 
+ * @author Mohammad AlMahamed 
  *
  */
+
+
  
 boolean checkPalindrome(String inputString) {   
  int i = 0;
