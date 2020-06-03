7c7aa89ce37447bdaa88d467f6b8b93b9d1c9e96
Mohammad Al-Mahamed <62557147+Mohammad515@users.noreply.github.com>
2020-06-03 13:48:44 +0200

    Update add.java

--- Intro/add.java
+++ Intro/add.java
@@ -2,12 +2,12 @@
  * Write a function that returns the sum of two numbers.
  *
  * Example For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3. 
- */
-/*
+ *
+ ********************************************************************************************
+ *
  * Schreiben Sie eine Funktion, die die Summe zweier Zahlen zurückgibt.
  * z.B Für param1 = 1 und param2 = 2 sollte die Ausgabe von add(param1,param2) = 3 sein.
- */
-/* 
+ * 
  * @author Mohammad AlMahamed 
  */
  
