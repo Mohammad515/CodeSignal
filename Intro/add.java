33c0777376d02c5a8d60aea65570ff01323bac9f
Mohammad Al-Mahamed <62557147+Mohammad515@users.noreply.github.com>
2020-06-03 14:01:06 +0200

    Update add.java

--- Intro/add.java
+++ Intro/add.java
@@ -6,6 +6,7 @@
  ********************************************************************************************
  *
  * Schreiben Sie eine Funktion, die die Summe zweier Zahlen zurückgibt.
+ *
  * z.B Für param1 = 1 und param2 = 2 sollte die Ausgabe von add(param1,param2) = 3 sein.
  * 
  * @author Mohammad AlMahamed 
