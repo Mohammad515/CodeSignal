ee0131990acd831c1c208b689150f3fcec1e6ae3
Mohammad AlMahamed <62557147+Mohammad515@users.noreply.github.com>
2021-07-09 16:00:23 +0200

    Create 42_bishopAndPawn.java

---
+++ Intro/42_bishopAndPawn.java
@@ -0,0 +1,7 @@
+boolean bishopAndPawn(String bishop, String pawn) {
+
+       if(bishop.charAt(0) - pawn.charAt(0) == bishop.charAt(1) - pawn.charAt(1) ||
+       bishop.charAt(0) - pawn.charAt(0) == -bishop.charAt(1) + pawn.charAt(1))
+        return true;
+    return false;
+}
