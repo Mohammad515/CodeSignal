d3e280f898e7664286f5c48e676527182e71d8ee
Mohammad AlMahamed <62557147+Mohammad515@users.noreply.github.com>
2021-07-09 15:58:11 +0200

    Create 39_knapsackLight.java

---
+++ Intro/39_knapsackLight.java
@@ -0,0 +1,14 @@
+int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
+
+  int value = 0;
+    if((weight1 <= maxW && (value1 > value2 || weight2 > maxW)) || (weight1 + weight2 <= maxW))
+    {
+        value += value1;
+        maxW -= weight1;
+    }
+    if(weight2 <= maxW)
+    {
+        value += value2;
+        maxW -= weight2;
+    }
+    return value;
